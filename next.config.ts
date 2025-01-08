cb0f78520aa2ae2c7c6a7eeb03c8f172586da2ce
Marco Mendes <87984109+marcomendesdev@users.noreply.github.com>
2025-01-08 20:26:57 +0000

    Update Clerk publishable key environment variable in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
+// filepath: /c:/Users/marco/OneDrive/Área de Trabalho/projects/new/newb/next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   },
   /* other config options here */
 };
