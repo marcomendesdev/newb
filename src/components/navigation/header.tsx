7b5bfaa98f85b40fb094f5c8a3addb620ace57c3
Marco Mendes <87984109+marcomendesdev@users.noreply.github.com>
2025-01-10 10:42:01 +0000

    Refactor header component to remove redundant 'Tables' link and update 'Book a table' link

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -48,13 +48,7 @@ export default function Header() {
               >
                 About
               </Link>
-              <Link
-                href="/tables"
-                className="flex w-full items-center py-2 text-lg font-semibold"
-                prefetch={false}
-              >
-                Tables
-              </Link>
+              
               <SignedIn>
                 <Link
                   href="/my-bookings"
@@ -78,7 +72,7 @@ export default function Header() {
                 <SignedIn>
                   <UserButton />
                 </SignedIn>
-                <Link href={"/book-table"} prefetch={false}>
+                <Link href="/tables" prefetch={false}>
                   <Button className="w-full">Book a table</Button>
                 </Link>
               </div>
@@ -110,13 +104,7 @@ export default function Header() {
               </Link>
             </NavigationMenuLink>
             <NavigationMenuLink asChild>
-              <Link
-                href="/tables"
-                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-                prefetch={false}
-              >
-                Tables
-              </Link>
+              
             </NavigationMenuLink>
             <SignedIn>
               <NavigationMenuLink asChild>
@@ -141,7 +129,7 @@ export default function Header() {
           </NavigationMenuList>
         </NavigationMenu>
         <div className="ml-auto lg:flex gap-2 hidden">
-          <Link href={"/book-table"} prefetch={false}>
+          <Link href="/tables" prefetch={false}>
             <Button>Book a table</Button>
           </Link>
           <SignedOut>
