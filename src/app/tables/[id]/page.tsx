36f3b6ad8cba482e9501a2121a06150a9868f2f0
Marco Mendes <87984109+marcomendesdev@users.noreply.github.com>
2025-01-11 21:47:06 +0000

    Refactor BookTable function to simplify parameter handling and remove unnecessary async logic

--- src/app/tables/[id]/page.tsx
+++ src/app/tables/[id]/page.tsx
@@ -1,27 +1,18 @@
 import BookingCard from "@/components/booking-card";
 import { BookingsProvider } from "@/context/bookings-context";
-// import type { Metadata } from 'next'
- 
+
 type Props = {
-  params: Promise<{ id: string }>
-  
-}
+  params: { id: string };
+};
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const id = (await params).id;
-//   return {
-//     title: `Booking for ${id}`,
-//   };
-// }
-
-export default async function BookTable({ params }: Props) {
-  const id = (await params).id;
+export default function BookTable({ params }: Props) {
+  const { id } = params; 
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className="container mx-auto p-4">
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 py-12">
           <BookingsProvider initialBookings={[]}>
-            <BookingCard id={id}  />
+            <BookingCard id={id} />
             <pre>{id}</pre>
           </BookingsProvider>
         </div>
