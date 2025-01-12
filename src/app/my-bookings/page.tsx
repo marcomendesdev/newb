15097f28647ef15a49edfa780416b5a65ace6aa1
Marco Mendes <87984109+marcomendesdev@users.noreply.github.com>
2025-01-12 19:41:34 +0000

    Enhance layout spacing in MyBookings page for improved readability

--- src/app/my-bookings/page.tsx
+++ src/app/my-bookings/page.tsx
@@ -14,9 +14,9 @@ export default async function MyBookings() {
 
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      <main className="container mx-auto p-4">
+      <main className="container mx-auto px-4 py-12">
         <h1 className="text-4xl font-bold py-12">My Bookings</h1>
-        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 py-20">
           <MyBookingsCard bookings={booking} />
         </div>
       </main>
